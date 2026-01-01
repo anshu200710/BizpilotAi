7c4d52ff84c8c437f9555a704c2cdd7c15a374be
anshu2007/10 <anshu200710>
2026-01-01 22:10:21 +0530

    gemini model update

--- backend/config/gemini.js
+++ backend/config/gemini.js
@@ -11,7 +11,7 @@ export const generateAIReply = async (message, language = "English") => {
     const genAI = new GoogleGenerativeAI(apiKey);
 
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-pro",
+      model: "gemini-1.0-pro",
     });
 
     const prompt = `
